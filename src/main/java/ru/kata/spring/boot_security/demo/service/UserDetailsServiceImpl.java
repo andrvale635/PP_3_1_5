f08cfdb5c556134e72eee1b8077a7dc7011f4f76
Valiok <vall199823@mail.ru>
2022-12-26 16:32:41 +0300

    v3

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserDetailsServiceImpl.java
@@ -23,6 +23,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     @Transactional
+
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> optionalUser = Optional.ofNullable(userService.showUserByName(username));
         User user = optionalUser.orElseThrow(() -> new UsernameNotFoundException("User not found"));
